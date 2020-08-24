c4c84048f6caf1202f6864c7e37311fdf576ba07
EUTyrin <e_tyrin@eureca.ru>
2020-08-24 12:14:39 +0300

    Initial commit

---
+++ app/src/main/java/evgenyt/audiotales/MainActivity.java
@@ -0,0 +1,24 @@
+package evgenyt.audiotales;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mediaPlayer = MediaPlayer.create(this, R.raw.red_hat);
+    }
+
+    public void play(View view) {
+        mediaPlayer.start();
+    }
+
+}
