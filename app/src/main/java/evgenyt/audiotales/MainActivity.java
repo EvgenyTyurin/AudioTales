5af64b9eb74cf1b061eeba3d0e0e2f60061ddebe
EUTyrin <e_tyrin@eureca.ru>
2020-08-25 18:33:39 +0300

    seek bar

--- app/src/main/java/evgenyt/audiotales/MainActivity.java
+++ app/src/main/java/evgenyt/audiotales/MainActivity.java
@@ -4,21 +4,61 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
 
     private MediaPlayer mediaPlayer;
+    private SeekBar seekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mediaPlayer = MediaPlayer.create(this, R.raw.red_hat);
+        seekBar = findViewById(R.id.player_seek);
+        Handler handler = new Handler();
+        MainActivity.this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if(mediaPlayer != null){
+                    int mCurrentPosition = mediaPlayer.getCurrentPosition() / 1000;
+                    seekBar.setProgress(mCurrentPosition);
+                }
+                handler.postDelayed(this, 1000);
+            }
+        });
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (mediaPlayer != null && fromUser) {
+                    mediaPlayer.seekTo(progress * 1000);
+                }
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
     }
 
-    public void play(View view) {
-        mediaPlayer.start();
+    // Play/pause button click handler
+    public void playOrPause(View view) {
+        Button button = (Button) view;
+        if (mediaPlayer.isPlaying()) {
+            seekBar.setMax(mediaPlayer.getDuration() / 1000);
+            mediaPlayer.pause();
+            button.setText("Читать");
+        } else {
+            mediaPlayer.start();
+            button.setText("Пауза");
+        }
     }
 
 }
